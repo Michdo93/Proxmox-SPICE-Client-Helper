6c1ecfebef81ae021ddd8bf8e55bc311dfc36789
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2024-10-08 11:23:32 +0200

    Create virt_viewer.sh

---
+++ virt_viewer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Setze den Pfad zu Python und dem Skript im Home-Verzeichnis des aktuellen Benutzers
+PYTHON_PATH="/usr/bin/python3"  # Pfad zu Python
+SCRIPT_PATH="$HOME/virt_viewer.py"  # Home-Verzeichnis des aktuellen Benutzers
+
+# Führe das Python-Skript aus
+"$PYTHON_PATH" "$SCRIPT_PATH"
+
+# Warte auf eine Eingabe (optional)
+read -p "Press any key to continue..."
