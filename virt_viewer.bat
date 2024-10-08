121039085b0c6ca9293cc122128db8c3d231f202
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2024-10-08 12:04:40 +0200

    Update virt_viewer.bat

--- virt_viewer.bat
+++ virt_viewer.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Setze den Pfad zu Python und dem Skript im Home-Verzeichnis des aktuellen Benutzers
 set PYTHON_PATH=C:\Python312\python.exe
-set SCRIPT_PATH=%USERPROFILE%\virt_viewer.py  REM Home-Verzeichnis des aktuellen Benutzers
+set SCRIPT_PATH=%USERPROFILE%\Proxmox-SPICE-Client-Helper\virt_viewer.py  REM Home-Verzeichnis des aktuellen Benutzers
 
 REM Führe das Python-Skript aus
 "%PYTHON_PATH%" "%SCRIPT_PATH%"
